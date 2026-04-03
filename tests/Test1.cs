62707bf057b322f2d43b2a114e6a366dd0c74be1
DIEMARIK <160759041+DIEMARIK@users.noreply.github.com>
2026-04-03 09:26:30 +0300

    Update Test1.cs

--- tests/Test1.cs
+++ tests/Test1.cs
@@ -39,99 +39,139 @@ namespace lab9._1
         }
     }
     [TestClass]
-public class AuthenticationServiceTests
-{
-    [TestMethod]
-    public void RegisterUser_ShouldCreateUser()
+    public class AuthenticationServiceTests
     {
-        var service = new AuthenticationService();
-        var user = service.RegisterUser("user1", "1234");
-
-        Assert.IsNotNull(user);
-        Assert.AreEqual("user1", user.Username);
-    }
-
-    [TestMethod]
-    public void RegisterUser_ShouldThrowInvalidOperationException_IfUserExists()
-    {
-        var service = new AuthenticationService();
-        service.RegisterUser("user1", "1234");
-
-        try
+        [TestMethod]
+        public void RegisterUser_ShouldCreateUser()
         {
+            var service = new AuthenticationService();
+            var user = service.RegisterUser("user1", "1234");
+
+            Assert.IsNotNull(user);
+            Assert.AreEqual("user1", user.Username);
+        }
+
+        [TestMethod]
+        public void RegisterUser_ShouldThrowInvalidOperationException_IfUserExists()
+        {
+            var service = new AuthenticationService();
             service.RegisterUser("user1", "1234");
-            Assert.Fail("Exception was not thrown");
+
+            try
+            {
+                service.RegisterUser("user1", "1234");
+                Assert.Fail("Exception was not thrown");
+            }
+            catch (InvalidOperationException)
+            {
+                Assert.IsTrue(true);
+            }
         }
-        catch (InvalidOperationException)
+
+        [TestMethod]
+        public void Login_ShouldThrowUnauthorizedAccessException_ForWrongPassword()
         {
-            Assert.IsTrue(true);
+            var service = new AuthenticationService();
+            service.RegisterUser("user1", "1234");
+
+            try
+            {
+                service.Login("user1", "0000");
+                Assert.Fail("Exception was not thrown");
+            }
+            catch (UnauthorizedAccessException)
+            {
+                Assert.IsTrue(true);
+            }
+        }
+
+        [TestMethod]
+        public void Login_ShouldThrowInvalidOperationException_IfUserNotFound()
+        {
+            var service = new AuthenticationService();
+
+            try
+            {
+                service.Login("unknown", "1234");
+                Assert.Fail("Exception was not thrown");
+            }
+            catch (InvalidOperationException)
+            {
+                Assert.IsTrue(true);
+            }
+        }
+        [TestMethod]
+        public void ChangeUserRole_ShouldChangeRole_WhenAdmin()
+        {
+            var service = new AuthenticationService();
+
+            var admin = service.RegisterUser("admin", "1234");
+            admin.ChangeRole(UserRole.Admin);
+
+            var user = service.RegisterUser("user1", "1234");
+
+            service.ChangeUserRole(admin, user, UserRole.Admin);
+
+            Assert.AreEqual(UserRole.Admin, user.Role);
+        }
+
+        [TestMethod]
+        public void ChangeUserRole_ShouldThrowUnauthorizedAccessException_WhenNotAdmin()
+        {
+            var service = new AuthenticationService();
+
+            var user1 = service.RegisterUser("user1", "1234");
+            var user2 = service.RegisterUser("user2", "1234");
+
+            try
+            {
+                service.ChangeUserRole(user1, user2, UserRole.Admin);
+                Assert.Fail("Exception was not thrown");
+            }
+            catch (UnauthorizedAccessException)
+            {
+                Assert.IsTrue(true);
+            }
         }
     }
-
-    [TestMethod]
-    public void Login_ShouldThrowUnauthorizedAccessException_ForWrongPassword()
+    [TestClass]
+    public class IntegrationTests
     {
-        var service = new AuthenticationService();
-        service.RegisterUser("user1", "1234");
+        private const string TestLogPath = "test_log.txt";
 
-        try
+        [TestInitialize]
+        public void Setup()
         {
-            service.Login("user1", "0000");
-            Assert.Fail("Exception was not thrown");
+            // Удаляем файл перед тестом, если существует
+            if (File.Exists(TestLogPath))
+                File.Delete(TestLogPath);
         }
-        catch (UnauthorizedAccessException)
+
+        [TestMethod]
+        public void BigBang_UpdateUser_ShouldUpdatePasswordAndRole_AndWriteLog()
         {
-            Assert.IsTrue(true);
+            var authService = new AuthenticationService();
+            var logService = new FileLogService(TestLogPath);
+            var updater = new UserUpdater(authService, logService);
+
+            // создаём пользователя
+            authService.RegisterUser("user1", "oldpass");
+
+            // обновляем
+            var updatedUser = updater.Update("user1", "newpass", UserRole.Admin);
+
+            Assert.AreEqual(UserRole.Admin, updatedUser.Role);
+            Assert.IsTrue(updatedUser.ValidatePassword("newpass"));
+            Assert.IsFalse(updatedUser.ValidatePassword("oldpass"));
+
+            Assert.IsTrue(File.Exists(TestLogPath));
         }
-    }
 
-    [TestMethod]
-    public void Login_ShouldThrowInvalidOperationException_IfUserNotFound()
-    {
-        var service = new AuthenticationService();
-
-        try
+        [TestCleanup]
+        public void Cleanup()
         {
-            service.Login("unknown", "1234");
-            Assert.Fail("Exception was not thrown");
-        }
-        catch (InvalidOperationException)
-        {
-            Assert.IsTrue(true);
-        }
-    }
-    [TestMethod]
-    public void ChangeUserRole_ShouldChangeRole_WhenAdmin()
-    {
-        var service = new AuthenticationService();
-
-        var admin = service.RegisterUser("admin", "1234");
-        admin.ChangeRole(UserRole.Admin);
-
-        var user = service.RegisterUser("user1", "1234");
-
-        service.ChangeUserRole(admin, user, UserRole.Admin);
-
-        Assert.AreEqual(UserRole.Admin, user.Role);
-    }
-
-    [TestMethod]
-    public void ChangeUserRole_ShouldThrowUnauthorizedAccessException_WhenNotAdmin()
-    {
-        var service = new AuthenticationService();
-
-        var user1 = service.RegisterUser("user1", "1234");
-        var user2 = service.RegisterUser("user2", "1234");
-
-        try
-        {
-            service.ChangeUserRole(user1, user2, UserRole.Admin);
-            Assert.Fail("Exception was not thrown");
-        }
-        catch (UnauthorizedAccessException)
-        {
-            Assert.IsTrue(true);
+            if (File.Exists(TestLogPath))
+                File.Delete(TestLogPath);
         }
     }
-}
 }
